14d953689cd3ad8c0259134f72bf18337935ef30
Hayimanot <haymimes@gmail.com>
2025-02-20 19:19:08 +0300

    Add UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 
 function App() {
@@ -21,6 +23,10 @@ function App() {
             <Header />
             <MainContent />
             <Footer />
+      </div>
+      <div>
+            <h1>Welcome to My App</h1>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
 
       <div>
