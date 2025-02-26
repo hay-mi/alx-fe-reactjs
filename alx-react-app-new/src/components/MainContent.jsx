3fd96da70c9f4d75e6563cf8a2c33cedfaa3c21c
Hayimanot <haymimes@gmail.com>
2025-02-26 16:25:38 +0300

    Added inline styling to components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function MainContent() {
+    return (
+        <main style={{ padding: '15px', fontSize: '1.2rem', textAlign: 'center' }} >
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
