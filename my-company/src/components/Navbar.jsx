4a55519453a1badf6b63285c3b329b13ebeabc98
Hayimanot <haymimes@gmail.com>
2025-03-02 12:17:24 +0300

    Initial commit: Set up React project with Vite

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ background: '#333', padding: '10px', color: 'white' }}>
+      <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'center' }}>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+        </li>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+        </li>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+        </li>
+        <li>
+          <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
