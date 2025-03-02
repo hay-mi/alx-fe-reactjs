98b77ae032655d101f73ba0b97a6b61a5219af92
Hayimanot <haymimes@gmail.com>
2025-03-02 22:07:27 +0300

    Apply inline CSS styling to components

--- my-company/src/components/Home.jsx
+++ my-company/src/components/Home.jsx
@@ -5,7 +5,6 @@ function Home() {
     <div style={{
       padding: '20px', textAlign: 'center',
       textAlign: 'center',
-      backgroundImage: 'url("https://images.unsplash.com/photo-1542281286-9e0a16bb134f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")', // Example background image
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       minHeight: '500px', 
