cfbbde61bc6f8b78f6c29846a890d268a4c82f17
Hayimanot <haymimes@gmail.com>
2025-03-05 20:36:11 +0300

    Implement user registration form with formik and yup

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -4,14 +4,20 @@ const RegistrationForm = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (username && email && password) {
-      // Here you would typically make an API call to register the user
+    let formErrors = {};
+
+    if (!username) formErrors.username = "Username is required";
+    if (!email) formErrors.email = "Email is required";
+    if (!password) formErrors.password = "Password is required";
+
+    setErrors(formErrors);
+
+    if (Object.keys(formErrors).length === 0) {
       console.log("User registered:", { username, email, password });
-    } else {
-      alert("Please fill in all fields.");
     }
   };
 
@@ -25,6 +31,7 @@ const RegistrationForm = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+        {errors.username && <div>{errors.username}</div>} {/* Display error */}
       </div>
       <div>
         <label htmlFor="email">Email:</label>
@@ -34,6 +41,7 @@ const RegistrationForm = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {errors.email && <div>{errors.email}</div>} {/* Display error */}
       </div>
       <div>
         <label htmlFor="password">Password:</label>
@@ -43,6 +51,7 @@ const RegistrationForm = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {errors.password && <div>{errors.password}</div>} {/* Display error */}
       </div>
       <button type="submit">Register</button>
     </form>
