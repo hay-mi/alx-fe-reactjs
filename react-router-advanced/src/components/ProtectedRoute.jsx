be148f58e563ee16d49dcfb7d8439da13a58336d
Hayimanot <haymimes@gmail.com>
2025-03-09 16:32:30 +0300

    Updated Advanced routing with React Router implemented.

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from ".src/hooks/useAuth";
 
-function ProtectedRoute({ isAuthenticated }) {
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+function ProtectedRoute() {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
