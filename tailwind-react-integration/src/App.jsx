8db3ddfbdb2494373acaac11d48a48e1353f1621
Hayimanot <haymimes@gmail.com>
2025-03-17 09:12:02 +0300

    Configured Tailwind CSS v2 in Vite React project

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-200">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Hello, Tailwind CSS v2!
+        </h1>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
